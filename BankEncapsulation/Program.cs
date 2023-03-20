19b573b37230f12a87b6c6fb353f8bd2ff345a6b
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-20 11:16:14 -0500

    completed

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace BankEncapsulation
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            var juansAccount = new BankAccount();
+            Console.WriteLine($"Welcome! Thank you for banking with us. Your account balance is ${juansAccount.GetBalance()}.");
+            var answer = "Y";
+            while (answer != "N")
+            {
+                Console.WriteLine("Would you like to deposit?\nType 'Y' if yes.\nType 'N' if no.");
+                answer = Console.ReadLine().ToUpper();
+                switch (answer)
+                {
+                    case "Y":
+                        Console.WriteLine("How much money would you like to deposit");
+                        var amountDeposit = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"\nYou have ${juansAccount.Deposit(amountDeposit)} in your bank account");
+                        break;
+                    case "N":
+                        Console.WriteLine($"Thank you for banking with us. You final balance is ${juansAccount.GetBalance()}.");
+                        break;
+                }
+            }
         }
     }
 }
